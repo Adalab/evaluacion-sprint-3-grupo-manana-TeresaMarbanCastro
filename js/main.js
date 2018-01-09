93bbfb0a6eabf731cd535446e67b599adf8150e4
Teresa Marban <teresamarbancastro@gmail.com>
2018-01-09 10:23:29 +0100

    menu show and hide OK

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 'use strict';
+//Display menu
+var show = document.querySelector('.menu-trigger');
+var showList = document.querySelector('.showCompleteMenu');
+var hideMenu = document.querySelector('.menu-close');
+
+function openMenu(){
+	showList.classList.add('show');
+	hideMenu.classList.add('closingMenu');
+}
+show.addEventListener('click', openMenu);
+
+//Hide Menu
+
+function closeMenu(){
+	showList.classList.remove('show');
+	hideMenu.classList.remove('closingMenu');
+}
+hideMenu.addEventListener('click', closeMenu);
